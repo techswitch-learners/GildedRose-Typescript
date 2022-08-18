d32049919a137969f85a57c6cd2e86f691c64c42
Konstantinos <kon.xen@gmail.com>
2022-08-18 14:40:49 +0100

    first "if"

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -19,12 +19,8 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
+            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality =  Math.max( this.items[i].quality-1, 0 );
             } else {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1
