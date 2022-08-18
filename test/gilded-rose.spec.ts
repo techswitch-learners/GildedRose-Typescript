ea7bf3a864f1c51a01e7d41b5fd4c13dbafc6e10
tomscambler <tomscambler@gmail.com>
2022-08-18 13:32:59 +0100

    added some empty tests

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,18 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
-    });
+    // it('should foo', function() {
+    //     const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    //     const items = gildedRose.updateQuality();
+    //     expect(items[0].name).to.equal('fixme');
+    // });
 
+    it('finding out basic expectations', function() {
+        const gildedRose = new GildedRose([ new Item('cricket bat', 5, 100) ]);
+
+        const items = gildedRose.updateQuality();
+
+        expect(items[0].name).to.equal('cricket bat');
+        expect(items[0].sellIn).to.equal(4);
+    });
 });
